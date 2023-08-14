935ca9a16bbd08ed9c3a42ee4abe92d7982f99a4
Devochkanakolese <115488507+Devochkanakolese@users.noreply.github.com>
2023-08-14 18:55:35 +0300

    Добавили изменения

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import random
 
+# Компуктор загадывает число
 q = random.randint(0,10)
 w = 0
 
 while True:
     w += 1
     print("Попытка:", w)
+    # Требуем от юзноа ввести число
     a = int(input("Введите число:"))
     if a > q:
         print("Меньше")
